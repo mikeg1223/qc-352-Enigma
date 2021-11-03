2db39d9440072c84635e7742e12325c3f96320e5
Michael Grossman <michael.grossman066@qmail.cuny.edu>
2021-11-02 20:29:27 -0400

    updated git stupidity in main.py

--- UnEnigma/main.py
+++ UnEnigma/main.py
@@ -7,7 +7,6 @@ e = Enigma()
 
 e.setRotors(1, 0, 2, 0, 3, 0)
 
-'''
 total = 0
 for _ in range(10000):
     t = time.time()
@@ -17,21 +16,9 @@ for _ in range(10000):
 total /= 1000
 
 print("average decryption time over 10000 trials:", total)
-'''
 
 table = {}
 rots = [_+1 for _ in range(8)]
 ringPos = [_ for _ in range(26)]
 
-'''
-with open("storeDecrypts.txt", 'w') as file:
-    for x in rots:
-        for y in rots:
-            for z in rots:
-                for a in ringPos:
-                    for b in ringPos:
-                        for c in ringPos:
-                            e.setRotors(x,a,y,b,z,c)
-                            file.write(e.encryptString(cipherText))
-'''
-                            
+
