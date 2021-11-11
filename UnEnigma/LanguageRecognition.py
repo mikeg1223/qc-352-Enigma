346a91b4d75712e8872a6041d825cebf5356935a
Matin Nazamy <matin.nazamy50@qmail.cuny.edu>
2021-11-11 13:49:43 -0500

    more cleanup // adding in more tests to see how good bisinkov can determine plugs

--- UnEnigma/LanguageRecognition.py
+++ UnEnigma/LanguageRecognition.py
@@ -11,9 +11,7 @@ class LanguageRecognition:
     unigramTable = {}
     bigramTable = {}
     trigramTable = {}
-    UNIGRAM_TABLE_LOADED = False
-    BIGRAM_TABLE_LOADED = False
-    TRIGRAM_TABLE_LOADED = False
+
 
     def __init__(self):
         self.loadUnigramTable()
@@ -44,106 +42,108 @@ class LanguageRecognition:
                 LanguageRecognition.trigramTable[pair[0]] = float(pair[1])
         LanguageRecognition.TRIGRAM_TABLE_LOADED = True
 
-    def resetTables(self):
-        self.unigramTable = {}
-        self.bigramTable = {}
-        self.trigramTable = {}
 
-    # The following 3 functions are used to compute the scoring for different bigrams and trigrams, and write them to outFiles.
-    # When done once for a sample, they don't need to be performed again unless to re-compute.
+    # Functions to build out our n-gram tables. Only need to be done once for a sample file.
+    '''
+        def resetTables(self):
+            self.unigramTable = {}
+            self.bigramTable = {}
+            self.trigramTable = {}
 
-    def createTrigramScoring(self, filename: str):
-        outputFileName = "Resources/trigram_scores.txt"
-        tgrams = {}
-        trigramCount = 0
-        with open(filename, "r") as file:
-            temp = "no"
-            for line in file:
-                line = line.strip()
-                line = line.replace(" ", "")
-                if len(line) == 0: continue
-                if temp+line[0] in tgrams:
-                    tgrams[temp+line[0]] += 1
-                else:
-                    tgrams[temp+line[0]] = 1
-                if temp[1] + line[:2] in tgrams:
-                    tgrams[temp[1] + line[:2] ] += 1
-                else:
-                    tgrams[temp[1] + line[:2] ] = 1
-                trigramCount += 2
-                temp = line[-2:]
-                for x in range(len(line)-3):
-                    if line[x:x+3] in tgrams:
-                        tgrams[line[x:x+3]] +=1
+        def createTrigramScoring(self, filename: str):
+            outputFileName = "Resources/trigram_scores.txt"
+            tgrams = {}
+            trigramCount = 0
+            with open(filename, "r") as file:
+                temp = "no"
+                for line in file:
+                    line = line.strip()
+                    line = line.replace(" ", "")
+                    if len(line) == 0: continue
+                    if temp+line[0] in tgrams:
+                        tgrams[temp+line[0]] += 1
                     else:
-                        tgrams[line[x:x+3]] = 1
+                        tgrams[temp+line[0]] = 1
+                    if temp[1] + line[:2] in tgrams:
+                        tgrams[temp[1] + line[:2] ] += 1
+                    else:
+                        tgrams[temp[1] + line[:2] ] = 1
+                    trigramCount += 2
+                    temp = line[-2:]
+                    for x in range(len(line)-3):
+                        if line[x:x+3] in tgrams:
+                            tgrams[line[x:x+3]] +=1
+                        else:
+                            tgrams[line[x:x+3]] = 1
+                        trigramCount += 1
+
+                
+                    if line[-3:] in tgrams:
+                        tgrams[line[-3:]] +=1
+                    else:
+                        tgrams[line[-3:]] = 1
                     trigramCount += 1
+            
+            with open(outputFileName, "a") as file:
+                for key in tgrams:
+                    file.write(key + " " + "{:.8f}".format(tgrams[key]/trigramCount)+ "\n")
+        
+        def createBigramScoring(self, filename: str):
+            outputFileName = "Resources/bigram_scores.txt"
+            bgrams = {}
+            bigramCount = 0
+            with open(filename, "r") as file:
+                temp = "I"
+                for line in file:
+                    line = line.strip()
+                    line = line.replace(" ", "")
+                    if len(line) ==0: continue
+                    if temp+line[0] in bgrams:
+                        bgrams[temp+line[0]] += 1
+                    else:
+                        bgrams[temp+line[0]] = 1
+                    bigramCount += 1
+                    temp = line[-1]
+                    for x in range(len(line)-2):
+                        if line[x:x+2] in bgrams:
+                            bgrams[line[x:x+2]] +=1
+                        else:
+                            bgrams[line[x:x+2]] = 1
+                        bigramCount += 1
+                    
+                    if line[-2:] in bgrams:
+                            bgrams[line[-2:]] +=1
+                    else:
+                        bgrams[line[-2:]] = 1
+                    bigramCount += 1
 
             
-                if line[-3:] in tgrams:
-                    tgrams[line[-3:]] +=1
-                else:
-                    tgrams[line[-3:]] = 1
-                trigramCount += 1
-        
-        with open(outputFileName, "a") as file:
-            for key in tgrams:
-                file.write(key + " " + "{:.8f}".format(tgrams[key]/trigramCount)+ "\n")
-    
-    def createBigramScoring(self, filename: str):
-        outputFileName = "Resources/bigram_scores.txt"
-        bgrams = {}
-        bigramCount = 0
-        with open(filename, "r") as file:
-            temp = "I"
-            for line in file:
-                line = line.strip()
-                line = line.replace(" ", "")
-                if len(line) ==0: continue
-                if temp+line[0] in bgrams:
-                    bgrams[temp+line[0]] += 1
-                else:
-                    bgrams[temp+line[0]] = 1
-                bigramCount += 1
-                temp = line[-1]
-                for x in range(len(line)-2):
-                    if line[x:x+2] in bgrams:
-                        bgrams[line[x:x+2]] +=1
-                    else:
-                        bgrams[line[x:x+2]] = 1
-                    bigramCount += 1
-                
-                if line[-2:] in bgrams:
-                        bgrams[line[-2:]] +=1
-                else:
-                    bgrams[line[-2:]] = 1
-                bigramCount += 1
+            with open(outputFileName, "a") as file:
+                for key in bgrams:
+                    file.write(key + " " + "{:.8f}".format(bgrams[key]/bigramCount)+ "\n")
 
-        
-        with open(outputFileName, "a") as file:
-            for key in bgrams:
-                file.write(key + " " + "{:.8f}".format(bgrams[key]/bigramCount)+ "\n")
-
-    def createUnigramScoring(self, filename: str) -> float:
-        outputFileName = "Resources/unigram_scores.txt"
-        ugrams = {}
-        unigramCount = 0
-        with open(filename, "r") as file:
-            for line in file:
-                line = line.strip()
-                line = line.replace(" ", "")
-                for x in line:
-                    if x in ugrams:
-                        ugrams[x] +=1
-                    else:
-                        ugrams[x] = 1
-                    unigramCount += 1
-        
-        with open(outputFileName, "a") as file:
-            for key in ugrams:
-                file.write(key + " " + "{:.8f}".format(ugrams[key]/unigramCount)+ "\n")
+        def createUnigramScoring(self, filename: str) -> float:
+            outputFileName = "Resources/unigram_scores.txt"
+            ugrams = {}
+            unigramCount = 0
+            with open(filename, "r") as file:
+                for line in file:
+                    line = line.strip()
+                    line = line.replace(" ", "")
+                    for x in line:
+                        if x in ugrams:
+                            ugrams[x] +=1
+                        else:
+                            ugrams[x] = 1
+                        unigramCount += 1
+            
+            with open(outputFileName, "a") as file:
+                for key in ugrams:
+                    file.write(key + " " + "{:.8f}".format(ugrams[key]/unigramCount)+ "\n")
+    '''
 
 
+    # Functions to perform statistical tests on a given string.
     '''
         The following 6 functions are all statistical tests to give a string a 'score' in English
         
@@ -242,11 +242,145 @@ class LanguageRecognition:
 
         return output
 
+    
+
+    # Functions for testing our statistics
+
+    def tryTestPairs(self,numTrials):
+
+        with open("Tests/testPairs.txt","r") as f:
+            with open("Tests/output_testPairs.txt","w") as o:
+
+                testName = " All 5 Plugs BiSinkov\n"
+                o.write(testName)
+                print(testName)
+                e = Enigma()
+                count = 0
+                totalStrScore = 0.0
+                totalPlugScore = 0.0
+
+                for line in f:
+                    
+                    count += 1
+                    if(count > numTrials): break
+                    line = line.strip()
+                    words = line.split()
+                    cipher = words[2]
+                    rotorSettings = words[1][:9]
+
+                    e.wipe()
+                    e.setRotors(int(rotorSettings[0]), int(rotorSettings[1] + rotorSettings[2]), int(rotorSettings[3]), int(rotorSettings[4] + rotorSettings[5]), int(rotorSettings[6]), int(rotorSettings[7] + rotorSettings[8]))
+                    decrypt = e.encryptString(cipher)
+
+                        #Calculate best plugs return the resultPlugs and resultStr
+                    resultPlugs, resultStr = self.findBestPlugs( decrypt, rotorSettings, o, cipher)
+
+                        # Score this string score
+                    thisStrScore = self.strCompare(   words[0], resultStr )
+                    totalStrScore += thisStrScore
+
+                        # Score this plug score
+                    thisPlugScore = self.plugCompare( words[1][9:], resultPlugs)
+                    totalPlugScore += thisPlugScore
+                    o.write("-----> TruePlugs = " + words[1][9:] + "     StringScore: " + str(round(thisStrScore,2)) + "     PlugScore: " + str(round(thisPlugScore,2)))
+
+
+                o.write("\n\n AVG STR SCORE: " + str((totalStrScore/numTrials)) + " \n AVG PLUG SCORE: " + str(totalPlugScore/numTrials))
+                print("\n\n AVG STR SCORE: " + str(totalStrScore/numTrials) + " \n AVG PLUG SCORE: " + str(totalPlugScore/numTrials))
+
     '''
-        This function is the driver of LanguageRecognition
-        It runs the algorithm to solve for the best plugs 1 by 1 by using language recognition statistics.
-        The choice of statistic used can be changed 
+        def tryIOC(self):
+
+            with open("IOC_unigram/IC_uni_output.txt","r") as f:
+                with open("Output_LangRec_OnTop5000IOC.txt","w") as o:
+
+                    count = 0
+
+                    for line in f:
+
+                        count+=1
+                        if(count>5): return
+
+                        line = line.strip()
+                        words = line.split()
+                        decrypt = words[2]
+                        rotorSettings = words[0] 
+
+                        self.findBestPlugs( decrypt, rotorSettings, o)
+
+        def tryCribs(self):
+
+                fname = "../Resources/all_possible_Crib/allKDB_"
+                oname = "../Resources/tryPlugs_on_cribs/output_"
+
+                for pageNum in range(1,2):
+
+                    fileName = fname + str(pageNum).zfill(2) + ".txt"
+                    outName = oname + str(pageNum).zfill(2) + ".txt"
+
+                    with open(fileName, "r") as f:
+                        with open(outName, "w" ) as o:
+
+                            for line in f:
+                                
+                                line = line.strip()
+                                words = line.split()
+
+                                decrypt = words[6]
+                                rotorSettings = str(words[0]) + str(words[1]) + str(words[2])+ str(words[3]) + str(words[4]) + str(words[5]) 
+
+                                self.findBestPlugs( decrypt, rotorSettings, o)
     '''
+
+    def strCompare(self, s1: str, s2: str) -> float:
+        score = 0.0
+        for i in range(len(s1)):
+            if(s1[i] == s2[i]): score+=1
+        
+            # Returns a percentage of the matching characters of s1 that match s2
+        return score/len(s1)
+
+    def plugCompare(self, truePlugs: str, calcPlugs: list ):
+        score = 0.0
+
+        for pair in calcPlugs:
+
+            #Note: The reason for find()%2 ==0 is to ensure we correctly confirm that this plug was paired:
+            # For example:        ABCD             the plugs are [AB] and [CD]
+            #       But without confirming, we might find that BC is a plug and score that (incorrectly)
+                    # Check for this exact plug pairing in the string
+            if (truePlugs.__contains__(pair) and truePlugs.find(pair[0])%2 == 0): score += 2 
+                     # plug pairing in reverse. AB = BA
+            elif truePlugs.__contains__( str(pair[1] + pair[0]) ) and (truePlugs.find(pair[1])%2 == 0): score += 2  
+
+        return score/10
+
+    def makeHistogram(self, numTrials:int ):
+        with open("Tests/output_testPairs.txt","r") as f:
+            with open("Tests/histogram_strings_output.txt","w") as o:
+                count =0
+                f.readline()
+                f.readline()
+                histAry = { }
+                for line in f:
+                    count = count+1
+                    if(count > numTrials): break
+                    line = line.strip()
+                    words = line.split()
+                    value = round(float(words[-3]),2)
+                    if value in histAry: histAry[value] = histAry[value]+1
+                    else: histAry[value] =1
+
+                val = -0.01
+                while(val <= 1.00):
+                    val = round(val + .01,2)
+                    if val in histAry:
+                        o.write("\n" + str(val) + "[" + str(histAry[val]) + "]: " )
+                        for _ in range(histAry[val]):
+                            o.write( "+")
+
+    
+    # Function to determine the 5 best plugs for a given string.
 
     def findBestPlugs(self, decrypt, rotorSettings, outFile, cipher="egcvqcsahlfmctzgwwxikupvunrujaqimbxnwjhkwnxnisjaqbmouylcbxdnvdbvf"):
 
@@ -257,11 +391,13 @@ class LanguageRecognition:
         e = Enigma()
         e.setRotors(int(rotorSettings[0]), int(rotorSettings[1] + rotorSettings[2]), int(rotorSettings[3]), int(rotorSettings[4] + rotorSettings[5]), int(rotorSettings[6]), int(rotorSettings[7] + rotorSettings[8]))
         
-        baseLine = self.sinkovStatisticBigram(decrypt)
-
             # Repeat for 5 plug pairs
-        for _ in range(5):
+        for i in range(5):
 
+            if (i < 5): test = self.sinkovStatisticBigram
+            #else: test = self.sinkovStatisticTrigram
+
+            baseLine = test(decrypt)
             maxSoFar = baseLine
 
             # Iterate x from 'A' to 'Y'
@@ -282,7 +418,7 @@ class LanguageRecognition:
                     e.setSteckerboardPlug(alpha[x], alpha[y])
 
                     s = e.encryptString(cipher)
-                    score = self.sinkovStatisticBigram(s)
+                    score = test(s)
 
                         # If we found a new best plug on this trial
                     if (score > maxSoFar):
@@ -296,8 +432,8 @@ class LanguageRecognition:
             # If we couldn't find a single improvement among any steckerboard pairs
             # End the simulation now, no need to get all 5 plug pairs
             if(maxSoFar <= baseLine):
-                outFile.write("cipher -->  Rotor: " + rotorSettings + " --> " + decrypt + " --> Plugs:" + str(plugs) + " --->  " + lastString + "\n") 
-                return 
+                outFile.write("\ncipher -->  Rotor: " + rotorSettings + " --> " + decrypt + " --> Plugs:" + str(plugs) + " --->  " + lastString) 
+                return plugs,lastString
             
             #otherwise: add the best pair to the list of plugs, and remove them from the alphabet
             else:
@@ -306,85 +442,16 @@ class LanguageRecognition:
         
 
         # When completed with 5 rounds, return plugs
-        outFile.write("cipher -->  Rotor: " + rotorSettings + " --> " + decrypt + " --> Plugs:" + str(plugs) + " --->  " + lastString + "\n") 
+        outFile.write("\ncipher -->  Rotor: " + rotorSettings + " --> " + decrypt + " --> Plugs:" + str(plugs) + " --->  " + lastString )
+        return plugs,lastString 
 
 
-        '''
-        These methods are just used to run the findBestPlugs() function on different files.
-        We stored files with different formats so they need to be read in differently.
-        Each one of these functions is tailored to a different input feed
-        '''
-
-    def tryIOC(self):
-
-        with open("IOC_unigram/IC_uni_output.txt","r") as f:
-            with open("Output_LangRec_OnTop5000IOC.txt","w") as o:
-
-                count = 0
-
-                for line in f:
-
-                    count+=1
-                    if(count>5): return
-
-                    line = line.strip()
-                    words = line.split()
-                    decrypt = words[2]
-                    rotorSettings = words[0] 
-
-                    self.findBestPlugs( decrypt, rotorSettings, o)
-
-    def tryCribs(self):
-
-            fname = "../Resources/all_possible_Crib/allKDB_"
-            oname = "../Resources/tryPlugs_on_cribs/output_"
-
-            for pageNum in range(1,2):
-
-                fileName = fname + str(pageNum).zfill(2) + ".txt"
-                outName = oname + str(pageNum).zfill(2) + ".txt"
-
-                with open(fileName, "r") as f:
-                    with open(outName, "w" ) as o:
-
-                        for line in f:
-                            
-                            line = line.strip()
-                            words = line.split()
-
-                            decrypt = words[6]
-                            rotorSettings = str(words[0]) + str(words[1]) + str(words[2])+ str(words[3]) + str(words[4]) + str(words[5]) 
-
-                            self.findBestPlugs( decrypt, rotorSettings, o)
-
-    def tryTestPairs(self):
-
-        with open("Tests/testPairs.txt","r") as f:
-            with open("Tests/output_testPairs.txt","w") as o:
-
-                e = Enigma()
-                count = 0
-                for line in f:
-                    
-                    count += 1
-                    if(count > 5): return
-                    line = line.strip()
-                    words = line.split()
-                    cipher = words[2]
-                    rotorSettings = words[1][:9]
-
-                    e.wipe()
-                    e.setRotors(int(rotorSettings[0]), int(rotorSettings[1] + rotorSettings[2]), int(rotorSettings[3]), int(rotorSettings[4] + rotorSettings[5]), int(rotorSettings[6]), int(rotorSettings[7] + rotorSettings[8]))
-                    decrypt = e.encryptString(cipher)
-
-                    self.findBestPlugs( decrypt, rotorSettings, o, cipher)
-
-    def hillClimbing(self,input: str):
-        pass
 
 # program runs from below 
 if __name__ == "__main__":
+    numTrials = 1000
     start = time.time()
     l = LanguageRecognition()
-    l.tryTestPairs()
-    print(str((time.time() - start)/5) + " seconds per compute") 
+    #l.tryTestPairs(numTrials)
+    l.makeHistogram(numTrials)
+    print(str((time.time() - start)/numTrials) + " seconds per compute") 
